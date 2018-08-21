b435706efdf35a3fe0e7c32b11f0e2ef233574a8
fd <fede.atanasoff@gmail.com>
2018-08-21 00:06:52 -0300

    envio de mail funcionando y contenido completo

--- public/js/mail-script.js
+++ public/js/mail-script.js
@@ -1,31 +1,43 @@
-    // -------   Mail Send ajax
+// -------   Mail Send ajax
 
-     $(document).ready(function() {
-        var form = $('#myForm'); // contact form
-        var submit = $('.submit-btn'); // submit button
-        var alert = $('.alert-msg'); // alert div for show alert message
+$(document).ready(function() {
+  var form = $("#myForm"); // contact form
+  var submit = $(".submit-btn"); // submit button
+  var alert = $(".alert-msg"); // alert div for show alert message
 
-        // form submit event
-        form.on('submit', function(e) {
-            e.preventDefault(); // prevent default form submit
+  // form submit event
+  form.on("submit", function(e) {
+    e.preventDefault(); // prevent default form submit
 
-            $.ajax({
-                url: 'mail.php', // form action url
-                type: 'POST', // form submit method get/post
-                dataType: 'html', // request type html/json/xml
-                data: form.serialize(), // serialize form data
-                beforeSend: function() {
-                    alert.fadeOut();
-                    submit.html('Sending....'); // change submit button text
-                },
-                success: function(data) {
-                    alert.html(data).fadeIn(); // fade in response data
-                    form.trigger('reset'); // reset form
-                    submit.attr("style", "display: none !important");; // reset submit button text
-                },
-                error: function(e) {
-                    console.log(e)
-                }
-            });
-        });
+    $.ajax({
+      url: "/mail", // form action url
+      method: "POST", // form submit method get/post
+      // dataType: 'html', // request type html/json/xml
+      data: form.serialize(), // serialize form data
+      beforeSend: function() {
+        form.append(
+          "<div class='alert alert-primary text-center animated flash' role='alert'>Enviando mensaje...</div>"
+        );
+        // alert.fadeIn();
+        // submit.html("Sending...."); // change submit button text
+      },
+      success: function(data) {
+        form.find(".alert-primary").hide();
+        form.append(
+          "<div class='alert alert-success text-center' role='alert'><h4 class='alert-heading'>Mensaje enviado!</h4><hr><p>En breve nos pondremos en contacto contigo.</p></div>"
+        );
+        console.log(data);
+        // alert.html(data).fadeIn(); // fade in response data
+        form.trigger("reset"); // reset form
+        // submit.attr("style", "display: none !important"); // reset submit button text
+      },
+      error: function(e) {
+        console.log(e);
+        form.find(".alert-primary").hide();
+        form.append(
+          "<div class='alert alert-danger text-center animated flash' role='alert'>Hubo un error. Intente nuevamente</div>"
+        );
+      }
     });
+  });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const nodemailer = require("nodemailer");
 
 let json;
 fs.readFile("json/data.json", "utf-8", (err, data) => {
@@ -13,6 +14,45 @@ fs.readFile("json/data.json", "utf-8", (err, data) => {
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "QWERTY Repair", json: json });
+});
+
+router.post("/mail", (req, res, next) => {
+  let transporte = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "qwerty.repair.pc@gmail.com",
+      pass: "P1a2s3s4"
+    }
+  });
+
+  let mailOptions = {
+    from: "QWERTY Repair <qwerty.repair.pc@gmail.com>",
+    to: "qwerty.repair.pc@gmail.com",
+    subject: "Has recibido un mensaje desde QWERTY Repair",
+    text: `Has recibido un nuevo mensaje de:  
+      - Nombre: ${req.body.name} 
+      - Email: ${req.body.email}
+      - Asunto: ${req.body.subject} 
+      - Mensaje: ${req.body.message}`,
+    html: `<p>Has recibido un nuevo mensaje</p>
+              <ul><li> Nombre ${req.body.name}</li> 
+              <li>Email: ${req.body.email} </li>
+              <li>Asunto: ${req.body.subject} </li>
+              <li>Mensaje: ${req.body.message}</li></ul>`
+  };
+
+  transporte.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.log(err);
+      // res.render("error", { msg: "error al enviar el formulario" });
+    } else {
+      console.log("mensaje enviado " + info.response);
+      res.send(mailOptions.text);
+      // res.render("enviado", { msg: "el mensaje fue enviado" });
+    }
+
+    //res.redirect("/");
+  });
 });
 
 module.exports = router;
